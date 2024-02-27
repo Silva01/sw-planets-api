7e9cd20c661e017579f6e3a2c125d1f6ed7be93c
silva01 <silva20102@gmail.com>
2024-02-27 07:24:25 -0300

    Initial Commit

---
+++ src/main/java/silva/daniel/project/study/sw_planets_api/SwPlanetsApiApplication.java
@@ -0,0 +1,13 @@
+package silva.daniel.project.study.sw_planets_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPlanetsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwPlanetsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/silva/daniel/project/study/sw_planets_api/SwPlanetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package silva.daniel.project.study.sw_planets_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPlanetsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
