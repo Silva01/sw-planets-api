18a8ac3b54607b9bd72daade56bcee12356de1fa
silva01 <silva20102@gmail.com>
2024-02-29 05:59:39 -0300

    Added Repository for Planet

---
+++ src/main/java/silva/daniel/project/study/sw_planets_api/domain/PlanetRepository.java
@@ -0,0 +1,6 @@
+package silva.daniel.project.study.sw_planets_api.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlanetRepository extends CrudRepository<Planet, Long> {
+}
