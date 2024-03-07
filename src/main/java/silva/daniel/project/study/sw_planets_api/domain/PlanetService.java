a8ad40a24ae2e03afaa6b13e0115b5fc088d854b
Daniel Silva <silva20102@gmail.com>
2024-03-07 12:42:42 -0300

    Update PlanetService.java

--- src/main/java/silva/daniel/project/study/sw_planets_api/domain/PlanetService.java
+++ src/main/java/silva/daniel/project/study/sw_planets_api/domain/PlanetService.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 public class PlanetService {
 
     private final PlanetRepository planetRepository;
+    private String teste = "";
 
     public PlanetService(PlanetRepository planetRepository) {
         this.planetRepository = planetRepository;
