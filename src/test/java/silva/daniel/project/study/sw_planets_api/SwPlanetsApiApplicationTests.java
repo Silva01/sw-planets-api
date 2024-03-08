f97532289635940bf3cfe70028333bc9078a326b
silva01 <silva20102@gmail.com>
2024-03-08 19:41:12 -0300

    Adicionado testes subcutâneos

--- src/test/java/silva/daniel/project/study/sw_planets_api/SwPlanetsApiApplicationTests.java
+++ src/test/java/silva/daniel/project/study/sw_planets_api/SwPlanetsApiApplicationTests.java
@@ -1,13 +1,37 @@
 package silva.daniel.project.study.sw_planets_api;
 
+import org.assertj.core.api.Assert;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import silva.daniel.project.study.sw_planets_api.domain.Planet;
+
+import static silva.daniel.project.study.sw_planets_api.commons.PlanetCommonsConstantEnum.PLANET;
+
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SwPlanetsApiApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    void createPlanet_ReturnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody()).isNotNull();
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
     }
 
 }
